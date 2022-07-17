c8dd6c197bc9da2f987164939c688105e237e9f3
YeeshaDev <101475738+YeeshaDev@users.noreply.github.com>
2022-07-17 12:06:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Simple Word Count</h1> 
+     <div className='underline'></div>
+     <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+export default function Form(){
+    const [input,setInput] = useState("")
+    const [count,setCount] = useState(0)
+    const [charCount,setCharCount] = useState(0)
+    const [maxLen,setMaxLen] = useState("")
+
+function handleSubmit(e){
+e.preventDefault();
+
+ }
+ 
+const wordChange =(e)=> {
+wordcount(e.target.value)
+setInput(e.target.value)
+console.log(input)
+
+}
+
+function wordcount(str){
+    let c = 0;
+    let str1 = str.split(" ");
+    charCnt(str);
+
+    for (let i = 0; i < str1.length; i++) {
+      if (str1.length === 0) {
+        setCount(0);
+      }
+      if (str1[i] !== "") {
+        c++;
+      }
+      setCount(c);
+    }
+  }
+
+  function charCnt(str) {
+    let c = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== " ") {
+        c++;
+      }
+    }
+    setCharCount(c);
+  }
+
+  function handleClick(e) {
+   setInput("")
+  setCount(0);
+setCharCount(0);
+setMaxLen("")
+  }
+
+    return (
+        <main>
+        <form className="word-form" onSubmit={handleSubmit}>
+            <label htmlFor="count">Word Limit:</label>
+            <input type="number"
+             placeholder="Input no of words"
+             value={maxLen}
+             onChange={(e) => setMaxLen(e.target.value)}
+             />
+           <textarea type="text" cols ="50" rows="20"
+           placeholder="Write your text here.."
+           className="text--input"
+           onChange={wordChange}
+           maxLength={maxLen}
+           value={input}>
+            </textarea> 
+           <p>Number of words: {count} words</p>
+           <p>Number of charcters: {charCount} characters</p>
+           <button onClick={handleClick}>
+            Clear Text
+           </button>
+        </form>
+        </main>
+    )
+}
